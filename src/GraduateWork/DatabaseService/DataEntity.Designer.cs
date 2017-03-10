32fb58378e0afb94b20084b86bb000be38293081
nazar.kravchuk <nazar.kravchuk@terrasoft.net.ua>
2017-03-10 21:05:26 +0200

    db change

--- src/GraduateWork/DatabaseService/DataEntity.Designer.cs
+++ src/GraduateWork/DatabaseService/DataEntity.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\__it\Source\Repos\GraduateWork\src\GraduateWork\DatabaseService\DataEntity.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\naziway\Source\Repos\GraduateWork\src\GraduateWork\DatabaseService\DataEntity.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
