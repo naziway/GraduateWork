eeb0b0986f63c49c3608a94f7dc36bb440029d5b
NazarKravchuk <Nazar.kravchuk@ukr.net>
2017-05-18 17:27:00 +0300

    проведення обстеження

--- src/GraduateWork/ViewModel/ReviewToOrderViewModel.cs
+++ src/GraduateWork/ViewModel/ReviewToOrderViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using DatabaseService;
 using Model;
 using PropertyChanged;
+using Shared;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace ViewModel
 {
@@ -10,21 +12,34 @@ namespace ViewModel
     {
         public DataService DataService { get; set; }
         public Review Review { get; set; }
-        public Device Device { get; set; }
-        public User Worker { get; set; }
+        public Part SelectedPart { get; set; }
+        public Work SelectedWork { get; set; }
 
-        ObservableCollection<Repair> Repairs { get; set; } = new ObservableCollection<Repair>();
-        ObservableCollection<Part> Parts { get; set; }
-        ObservableCollection<Works> Works { get; set; }
+
+        public ObservableCollection<Repair> Repairs { get; set; } = new ObservableCollection<Repair>();
+        public ObservableCollection<Part> Parts { get; set; }
+        public ObservableCollection<Work> Works { get; set; }
 
         public ReviewToOrderViewModel(DataService dataService, Review review)
         {
             DataService = dataService;
             Review = review;
+
+            Parts = new ObservableCollection<Part>(DataService.GetParts());
+            Works = new ObservableCollection<Work>(DataService.GetWorks());
+
         }
 
 
+        public ICommand AddRepair => new CommandHandler(() =>
+        {
+            Repairs.Add(new Repair
+            {
+                Work = SelectedWork,
+                Part = SelectedPart,
+            });
 
+        });
 
 
     }
